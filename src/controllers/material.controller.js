074231df3fa7f0ed9d7ceea56e06bb92c8d446a1
Natalia <nbedoyaruiz@gmail.com>
2021-08-10 16:52:35 -0500

    MVC material

--- src/controllers/material.controller.js
+++ src/controllers/material.controller.js
@@ -20,40 +20,14 @@ module.exports = {
     }
   },
 
-  async update(req, res) {
-    try {
-      // const { materialId, body } = req;
-      const {
-        params: { materialId },
-        body,
-      } = req;
-      const material = await Material.findByIdAndUpdate(materialId, body, {
-        new: true,
-      });
-      res.status(200).json(material);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  },
-
   async destroy(req, res) {
     try {
-      //const { materialId } = req;
-      const { materialId } = req.params;
+      const { materialId } = req.body;
+
       const material = await Material.findByIdAndDelete(materialId);
       res.status(200).json(material);
     } catch (error) {
       res.status(400).json({ message: error.message });
     }
   },
-
-  // async show(req, res) {
-  //   try {
-  //     const { userId } = req;
-  //     const user = await User.findById(userId);
-  //     res.status(200).json(user);
-  //   } catch (error) {
-  //     res.status(400).json({ message: error.message });
-  //   }
-  // },
 };
--- src/routes/material.js
+++ src/routes/material.js
@@ -2,10 +2,7 @@ const router = require("express").Router();
 const materialController = require("../controllers/material.controller");
 
 router.route("/create").post(materialController.create);
-router.route("/list").get(materialController.list);
-router.route("/:materialId").put(materialController.update);
-//router.route("/update").put(materialController.update);
-router.route("/:materialId").delete(materialController.destroy);
-//router.route("/delete").delete(materialController.destroy);
+router.route("/materialList").get(materialController.list);
+router.route("/materialDelete").delete(materialController.destroy);
 
 module.exports = router;
