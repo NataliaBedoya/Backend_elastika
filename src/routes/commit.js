a22ccd4fb9747e19e28553e8bbc18a856fbfaaa3
Natalia <nbedoyaruiz@gmail.com>
2021-08-21 09:09:56 -0500

    Corrige la ruta para listar los commits

--- src/routes/commit.js
+++ src/routes/commit.js
@@ -2,8 +2,7 @@ const router = require("express").Router();
 const commitController = require("../controllers/commit.controller");
 
 router.route("/create").post(commitController.create);
-router.route("/commitist").get(commitController.list);
+router.route("/commitList").get(commitController.list);
 router.route("/commitDelete").delete(commitController.destroy);
 
 module.exports = router;
-s;
